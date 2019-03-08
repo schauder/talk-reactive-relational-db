ac6bc62f22c27cae0bf2b16c0f1bac55b248503e
Jens Schauder <jschauder@pivotal.io>
2019-03-08 13:26:36 +0100

    created project for demo code

---
+++ reactive-relational/src/main/java/de/schauderhaft/reactiverelational/ReactiveRelationalApplication.java
@@ -0,0 +1,13 @@
+package de.schauderhaft.reactiverelational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRelationalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveRelationalApplication.class, args);
+	}
+
+}
---
+++ reactive-relational/src/test/java/de/schauderhaft/reactiverelational/ReactiveRelationalApplicationTests.java
@@ -0,0 +1,16 @@
+package de.schauderhaft.reactiverelational;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveRelationalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
