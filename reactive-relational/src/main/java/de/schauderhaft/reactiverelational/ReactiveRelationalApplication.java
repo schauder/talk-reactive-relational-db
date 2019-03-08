5ec3256451a4ee0e3cce6ce0d56ee62a27107f76
Jens Schauder <jschauder@pivotal.io>
2019-03-08 14:14:45 +0100

    First actual database interaction

--- reactive-relational/src/main/java/de/schauderhaft/reactiverelational/ReactiveRelationalApplication.java
+++ reactive-relational/src/main/java/de/schauderhaft/reactiverelational/ReactiveRelationalApplication.java
@@ -3,11 +3,13 @@ package de.schauderhaft.reactiverelational;
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.data.r2dbc.function.DatabaseClient;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
@@ -35,13 +37,29 @@ class BricksConfiguration extends AbstractR2dbcConfiguration {
 						.build());
 
 	}
+
 }
 
 @Component
 class Demo implements CommandLineRunner {
 
+	@Autowired
+	DatabaseClient client;
+
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World");
+
+		client.execute()
+				.sql("DROP TABLE IF EXISTS BRICK; " +
+						"CREATE TABLE BRICK( ID SERIAL PRIMARY KEY, NAME VARCHAR(200), HEIGHT INTEGER, WIDTH INTEGER, LENGTH INTEGER)")
+				.then()
+				.doAfterTerminate(() -> System.out.println("done"))
+				.block();
+
+		System.out.println("done done");
+
+
+
 	}
 }
