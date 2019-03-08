371d6242604d032747e018349c6f95c19a058c0d
Jens Schauder <jschauder@pivotal.io>
2019-03-08 13:56:28 +0100

    connection setup

--- reactive-relational/src/main/java/de/schauderhaft/reactiverelational/ReactiveRelationalApplication.java
+++ reactive-relational/src/main/java/de/schauderhaft/reactiverelational/ReactiveRelationalApplication.java
@@ -1,7 +1,14 @@
 package de.schauderhaft.reactiverelational;
 
+import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
+import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class ReactiveRelationalApplication {
@@ -10,4 +17,31 @@ public class ReactiveRelationalApplication {
 		SpringApplication.run(ReactiveRelationalApplication.class, args);
 	}
 
+}
+
+
+@Configuration
+class BricksConfiguration extends AbstractR2dbcConfiguration {
+
+
+	@Override
+	public ConnectionFactory connectionFactory() {
+
+		return new PostgresqlConnectionFactory(
+				PostgresqlConnectionConfiguration.builder()
+						.host("localhost")
+						.username("postgres")
+						.password("")
+						.build());
+
+	}
+}
+
+@Component
+class Demo implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World");
+	}
 }
